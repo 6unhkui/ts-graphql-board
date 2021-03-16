ce2f5df9b6102cc1a56a9dbfbfb6988e962bf370
inkyung <kyxxin@hanmail.net>
2021-03-16 15:16:20 +0900

    CONFIG : ADD .env

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,16 +2,21 @@ import { __prood__ } from "./constatns";
 import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default {
+  type: "postgresql",
+  host: "127.0.0.1",
+  port: 5432,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  dbName: "board",
   migrations: {
     path: path.join(__dirname, "./migrations"),
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
   entities: [Post],
-  user: "inkyung",
-  password: "pass",
-  dbName: "board",
-  type: "postgresql",
   debug: !__prood__,
 } as Parameters<typeof MikroORM.init>[0];
